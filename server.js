0e1892a52d9d67a24730388b42af7de6ba2ee8eb
Nathan Vu <nathanvu99@gmail.com>
2020-10-16 19:21:35 -0700

    Added express post and delete

--- server.js
+++ server.js
@@ -3,38 +3,67 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const PORT = 2000;
+const PORT = process.env.PORT || 2000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "./public")));
 
-const writefileAsync = util.promisify(fs.writeFile);
-const readFileAsync = util.promisify(fs.readFile);
-
-let notes = [];
-let journalData = require('./journal');
-
+// HTML Routes
 app.get('/notes', function(req, res) {
-  res.sendFile(path.join(__dirname, '../public/notes.html'));
+  res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+// API Routes
 app.get('/api/notes', function(req, res) {
-  return res.json(notes);
+  res.sendFile(path.join(__dirname, "/db/db.json"));
 });
 
-app.get('./journal', function(req, res) {
-  return res.json(journalData);
+// Getting notes by ID
+app.get("/api/notes/:id", function(req, res) {
+  let savedNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+  res.json(savedNotes[Number(req.params.id)]);
 });
 
 app.post('/api/notes', function(req, res) {
+  let savedNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
   let noteEntry = req.body;
-  readFileAsync(path.join(__dirname, './journal.json'), 'utf8')
-    .then(function(response) {
-      notes = JSON.parse(response);
-    });
+  let newId = savedNotes.length;
+  noteEntry.id = newId;
+
+  console.log('Note ID is: ' + noteEntry.id);
+
+  savedNotes.push(noteEntry);
+
+  updateDb(savedNotes);
+  res.json(noteEntry);
+});
+
+app.delete("/api/notes/:id", function(req, res) {
+  let savedNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+  let noteID = req.params.id;
+  let newID = 0;
+  savedNotes.splice(noteID, 1);
+
+  for (note of savedNotes) {
+    note.id = newID;
+    newID++;
+  }
+  
+  fs.writeFileSync('./db/db.json', JSON.stringify(savedNotes));
+  res.json(noteID);
+})
+
+updateDb = (savedNotes) => {
+  console.log('updated db');
+  fs.writeFileSync('./db/db.json', JSON.stringify(savedNotes));
+}
+
+// Start the server on the port
+app.listen(PORT, function() {
+  console.log("SERVER IS LISTENING: " + PORT);
 });
